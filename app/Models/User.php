6bd46200bc8b002a197a6d61dc9bd20f32ce535f
phonglm <phonglm@phx-smartschool.com>
2023-10-19 23:23:02 +0700

    update: name model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,44 +2,37 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    protected $primaryKey = 'userID';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
+        'firstName',
+        'lastName',
+        'birthday',
         'email',
+        'address',
+        'gender',
+        'phoneNumber',
+        'username',
         'password',
+        'urlImage',
     ];
+      protected $hidden = [
+            'password',
+            'remember_token',
+        ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function projects()
+    {
+        return $this->hasMany(Project::class, 'projectCreator', 'userID');
+    }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
 }
