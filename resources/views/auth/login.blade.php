2d60665b66777eaded6adbe3108609759975eb15
phonglm <phonglm@phx-smartschool.com>
2023-10-21 23:19:54 +0700

    update : show error mess

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @vite('resources/css/app.css')
 
-<section class="">
-    <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0" style="width: 500px">
+<section class="" style="background-image: none; background-size: cover; background-repeat: no-repeat; background-attachment: fixed; width: 100%; height: 100vh;" >
+    <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0" style="width: 500px ;opacity: 0.9;">
         <a href="#" class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
             <img class="mr-2 h-16 w-16" src="https://cdn.discordapp.com/attachments/953496613878399026/1165023769551700078/logo_phenikaa_bugs_master.png?ex=654557ef&is=6532e2ef&hm=5c9bf5a363badbee958fac10b1b24632725cbf06475e0e1a5c23a0c53c93fb88&" alt="logo">
             Phenikaa Bugs Master
@@ -27,6 +27,13 @@
                             class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
                             required>
                     </div>
+
+                    @if ($errors->get('email'))
+                    <div class="flex items-center w-full">
+                        <p class="text-sm text-red-500	">Sai tài khoản hoặc mật khẩu</p>
+                    </div>
+                    @endif
+
                     <div class="flex items-center justify-between">
                         <div class="flex items-start">
                             <div class="flex h-5 items-center">
@@ -40,6 +47,7 @@
                         <a href="#"
                             class="text-primary-600 dark:text-primary-500 text-sm font-medium hover:underline">Quên mật khẩu?</a>
                     </div>
+
                     <button type="submit"
                         class="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg bg-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4">Sign
                         in</button>
